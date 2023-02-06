1564b180deb7d2ec60677f28ec33a7ecb8a59e3d
MANIKANTA325 <edupugantimanikanta143@gmail.com>
2023-02-06 13:39:00 +0530

    ignore

---
+++ equalsignorecase.java
@@ -0,0 +1,11 @@
+public class equalsignorecase {
+    public static void main(String args[]){
+        String s1="mani";
+        String s2="reddy";
+        String s3="kodi";
+        String s4="python";
+        System.out.println(s1.equalsIgnoreCase(s2));
+        System.out.println(s1.equalsIgnoreCase(s3));
+        System.out.println(s1.equalsIgnoreCase(s4));
+    }
+}
