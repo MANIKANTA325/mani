ee28f7023aeebaec635ae15b5e2ac752a6d8c825
MANIKANTA325 <edupugantimanikanta143@gmail.com>
2023-02-06 14:15:20 +0530

    trim

---
+++ stringtrim.java
@@ -0,0 +1,7 @@
+public class stringtrim {
+    public static void main(String args[]){
+        String s1="  hello string   ";
+        System.out.println(s1+"mani");
+        System.out.println(s1.trim()+"mani");
+    }
+}
