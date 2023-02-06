3db2a2435e6d2c57494f9e033b2b554e0d3e5ede
MANIKANTA325 <edupugantimanikanta143@gmail.com>
2023-02-06 13:30:30 +0530

    mani

---
+++ stringconcat.java
@@ -0,0 +1,9 @@
+public class stringconcat {
+    public static void main(String args[]){
+    String s1="java string";
+
+    System.out.println(s1);
+    s1=s1.concat(" is immutable so assign it explicitly");
+    System.out.println(s1);
+}
+}
