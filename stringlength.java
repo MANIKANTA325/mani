65d5c90daf78d5d3f9e8d0037d3e00ebd4a34875
MANIKANTA325 <edupugantimanikanta143@gmail.com>
2023-02-06 13:58:25 +0530

    length

---
+++ stringlength.java
@@ -0,0 +1,7 @@
+public class stringlength {
+    public static void main(String args[]){
+        String s1="mani";
+        String s2="narsi";
+        System.out.println("string length is: "+s1.length());
+        System.out.println("string length is: "+s2.length());
+    }}
