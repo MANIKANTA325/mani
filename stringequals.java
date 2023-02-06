5767ccadbb3b53e1d189852fd80156675d97af04
MANIKANTA325 <edupugantimanikanta143@gmail.com>
2023-02-06 13:36:06 +0530

    equals

---
+++ stringequals.java
@@ -0,0 +1,11 @@
+public class stringequals {
+    public static void main(String args[]){
+        String s1="mani";
+        String s2="surya";
+        String s3="durga";
+        String s4="python";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equals(s4));
+    }
+}
